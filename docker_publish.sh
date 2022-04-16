6718f02977c7b35f6189c49d930a248ea133181d
Victor Hugo <victorshlima@gmail.com>
2022-04-15 21:17:03 -0300

    docker

--- docker_publish.sh
+++ docker_publish.sh
@@ -1,6 +1,6 @@
-#victation/infnet-gitlab:1.0.5
+#victation/infnet-gitlab:1.0.6
 
-export VERSION=1.0.5
+export VERSION=1.0.6
 export TAG_NAME=$VERSION
 export LOCAL_IMAGE=victation/infnet-gitlab
 export NEW_REPO=victation/infnet-gitlab
